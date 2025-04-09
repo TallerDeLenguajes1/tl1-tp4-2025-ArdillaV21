277513aa90f2e9ef58ce04f3e9d64c223eea2f26
ArdillaV21 <fedevalle21@gmail.com>
2025-04-09 03:51:11 -0300

    Creo la funcion PedirTareas

--- tp4_dis.c
+++ tp4_dis.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define TAM_DESC 100
 
 struct Tarea{
     int TareaID; //Numerico autoincremental comenzando en 1000
@@ -9,12 +11,37 @@ struct Tarea{
 
 struct Nodo{
     Tarea T;
-    Nodo *Siguiente;
+    struct Nodo *Siguiente;
 
 }typedef Nodo;
 
-int main(){
+
+Tarea PedirTareas(){
+    int ID_incremental = 1000;
+    Tarea nueva;
+    nueva.TareaID = ID_incremental++;
+
+    char buffer[TAM_DESC];
+    printf("Ingrese la descripcion de la tarea: \n");
+    getchar();
+    fgets(buffer,TAM_DESC,stdin);
+    buffer[strcspn(buffer,"\n")] = 0;
+
+    nueva.Descripcion = (char *) malloc(strlen(buffer + 1) * sizeof(char));
+
+    do{
+        printf("Ingrese la duracion de la tarea: ");
+        scanf("%d", &nueva.Duracion);
+    }while(nueva.Duracion<10 || nueva.Duracion>100);
     
+    return nueva;
+}
+
+int main(){
+    Nodo *TareasPendientes = NULL;
+    Nodo *TareasRealizadas = NULL;
+
+    PedirTareas();
 
     return 0;
 }
